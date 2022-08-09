ee8874778c49f18a18877d9f55dd0d7f58b67f16
ImmanuelEgboche <77640433+ImmanuelEgboche@users.noreply.github.com>
2022-08-09 19:48:37 +0100

    adding transactions

--- blockchain.js
+++ blockchain.js
@@ -1,21 +1,40 @@
 const SHA256 = require("crypto-js/sha256");
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress
+        this.toAddress = toAddress
+        this.amount = amount
 
+    }
+}
 class Block{
-    constructor(index, timestap, data, previousHash = ''){
-        this.index = index;
+    constructor( timestap, transactions, previousHash = ''){
         this.timestap = timestap;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.hash = this.calculateHash()
+        this.nonce = 0 
+        // lol 
     }
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestap + JSON.stringify(this.data)).toString()
+        return SHA256(this.index + this.previousHash + this.timestap + JSON.stringify(this.data) + this.nonce).toString()
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce ++
+            this.hash = this.calculateHash()
+        }
+        console.log("Block mined:" + this.hash)
     }
 }
 
 class BlockChain{
     constructor(){
         this.chain = [this.createGenesisBlock()]
+        this.difficulty = 2
+        this.pendingTransactions = []
+        this.miningReward = 100
 
     }
     createGenesisBlock(){
@@ -24,15 +43,38 @@ class BlockChain{
     getLatestBlock(){
         return this.chain[this.chain.length - 1]
     }
-    addBlock(newBlock){
-        newBlock.previousHash = this.getLatestBlock().hash
-        newBlock.hash = newBlock.calculateHash()
-        this.chain.push(newBlock)
+    minePendingTransactions(miningRewardAddress){
+        let block = new Block(Date.now(), this.pendingTransactions)
+        // in the real world calling all pending transactions would take way too long and 
+        // block size cant be bigger than 1mb 
+        block.mineBlock(this.difficulty)
+        console.log("\n Block successfully mined")
+        this.chain.push(block)
+    }
+
+    // adding validity
+    isChainVaild(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i]
+            const previousBlock = this.chain[i - 1]
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false
+            }
+            if (currentBlock.previousHash !== previousBlock.hash){
+                return false
+            }
+            return true 
+
+        }
     }
 }
 
 let example = new BlockChain()
-example.addBlock(new Block(1,'31/07/22',{amount: 4}))
-example.addBlock(new Block(2,'31/07/22',{amount: 11}))
 
-console.log(JSON.stringify(example, null, 4))
+
+
+// adding to chain after block has been created
+
+
+// console.log(JSON.stringify(example, null, 4))
